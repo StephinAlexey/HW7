ece599a7677d7f86c104443a288d239e0c871fa1
StephinAlexey <alexeystephin@gmail.com>
2024-07-14 01:20:23 +0300

    Exercises 2 & 3 added

---
+++ exercise2/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 2: Напишите программу вычисления функции
+// Аккермана с помощью рекурсии. Даны два
+// неотрицательных числа m и n.
+// A(m, n) = 
+// {
+//     n + 1,                 если m = 0;
+//     A(m - 1, 1),           если m > 0 и n = 0;
+//     A(m - 1, A(m, n - 1)), если m > 0 и n > 0.
+// }
+
+
+// Пример
+// m = 2, n = 3 -> A(m,n) = 9
+
+void Main()
+{
+    int M = TakeNumFromUser("Please insert a first not negative number");
+    int N = TakeNumFromUser("Please insert a second not negative number");
+    System.Console.WriteLine($"The Akkerman function result is: {A(M,N)}");
+
+}
+
+int A(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    if (m > 0 && n == 0)
+        return A(m - 1, 1);
+    if (m > 0 && n > 0)
+        return A(m - 1, A(m, n - 1));
+    else
+    return 0;
+}
+
+int TakeNumFromUser(String TextForUser)
+{
+    while (true)
+    {
+        try
+        {
+            Console.WriteLine(TextForUser);
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num < 0)
+                Console.WriteLine("You must enter not negative number");
+            else
+                return num;
+
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("You must enter a valid iteger number");
+        }
+    }
+}
+Main();
---
+++ exercise3/Program.cs
@@ -0,0 +1,95 @@
+﻿// Задача 3: Задайте произвольный массив. Выведете
+// его элементы, начиная с конца. Использовать
+// рекурсию, не использовать циклы
+
+//Пример
+//[1, 2, 5, 0, 10, 34] => 34 10 0 5 2 1 
+
+
+void Main()
+{
+    int Size = GetArraySizeFromUser();
+    (int MinVal,int MaxVal) MinMaxVal = GetMinMaxValFromUser();
+    int[] Array = InitIntArray(Size,MinMaxVal.MinVal,MinMaxVal.MaxVal);
+    ArrayDisplay(Array,"The original Array is:");
+    ReverseArrayDisplay(Array, "The reverse Array is:");
+}
+
+
+
+int[] InitIntArray(int Size, int MinVal, int MaxVal)
+{
+    Random rnd = new();
+    int[] IntArray = new int[Size];
+    for (int i = 0; i < Size; i++)
+    {
+        IntArray[i] = rnd.Next(MinVal, MaxVal);
+    }
+    return IntArray;
+}
+(int MinVal, int MaxVal) GetMinMaxValFromUser()
+{
+    int MinVal = GetNumFromUser("Please enter the minimum value for random ");
+    int MaxVal;
+    while (true)
+    {
+        MaxVal = GetNumFromUser("Please enter the maximum value for random ");
+        if (MaxVal > MinVal)
+            break;
+        else
+            System.Console.WriteLine($"The maximum value must be bigger than {MinVal} ");
+    }
+    return (MinVal, MaxVal);
+}
+void ArrayDisplay(int[] IntArray, string DisplayText)
+{
+    System.Console.WriteLine(DisplayText);
+    foreach (int item in IntArray)
+    {
+        System.Console.Write(item + " ");
+
+    }
+    System.Console.WriteLine();
+}
+void ReverseArrayDisplay(int[] IntArray, string DisplayText)
+{
+    System.Console.WriteLine(DisplayText);
+    RecursiveArrayDisplay(IntArray,IntArray.Length-1);
+}
+void RecursiveArrayDisplay(int[] Array,int begin_index)
+{
+    if(begin_index<0)
+    return;
+    System.Console.Write(Array[begin_index]+ " ");
+    RecursiveArrayDisplay(Array,begin_index-1);
+}
+int GetNumFromUser(String TextForUser)
+{
+    while (true)
+    {
+        try
+        {
+            Console.WriteLine(TextForUser);
+            int num = Convert.ToInt32(Console.ReadLine());
+            return num;
+
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("You must enter a valid iteger number");
+        }
+    }
+}
+int GetArraySizeFromUser()
+{
+    int Size;
+    while (true)
+    {
+        Size = GetNumFromUser("Please enter array length");
+        if (Size > 0)
+            return Size;
+        else
+            System.Console.WriteLine("Size of array is not valid");
+    }
+}
+Main();
